e384144f03de3eca64e09bdc6afaa9dabbf3b36f
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-07-28 15:02:47 -0500

    use 1.0.4 and remove optional deps

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,13 @@
+# This hack is necessary because bazel produces a Python script with
+# "#! /usr/bin/python" at the top. On CI systems this is a problem,
+# but only on Linux.
+if [ "$(uname)" == "Linux" ]; then
+    if [! -f /usr/bin/python ] || [ -L /usr/bin/python ]; then
+        rm /usr/bin/python || true
+        ln -s %PYTHON% python
+    fi
+fi
+
 # Build the wheel.
 bazel build --subcommands //snowflake/ml:wheel
 
