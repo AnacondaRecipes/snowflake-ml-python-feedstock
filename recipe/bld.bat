12e3632df7efff2388660a436f65b5c1992503fb
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-08-07 14:56:30 -0500

    windows changes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,9 @@
 rem Build the wheel.
-bazel --output_user_root=C:\bazelcrap build --subcommands --verbose_failures --build_python_zip=false --enable_runfiles=true --action_env="USERPROFILE=%USERPROFILE%" --host_action_env="USERPROFILE=%USERPROFILE%" //snowflake/ml:wheel
+bazel --output_user_root=C:\bazelcrap build --subcommands --verbose_failures --build_python_zip=false --enable_runfiles=true --action_env="USERPROFILE=%USERPROFILE%" --host_action_env="USERPROFILE=%USERPROFILE%" --action_env="CONDA_DLL_SEARCH_MODIFICATION_ENABLE=1" --host_action_env="CONDA_DLL_SEARCH_MODIFICATION_ENABLE=1" //snowflake/ml:wheel
 
 rem Install it.
 FOR /F "delims=" %%i IN ('dir C:\bazelcrap /b /ad-h /t:w /od') DO SET a=%%i
-%PYTHON% -m pip install -vv --no-deps --no-build-isolation C:\bazelcrap\%a%\execroot\SnowML\bazel-out\x64_windows-fastbuild\bin\snowflake\ml\*.whl
+%PYTHON% -m pip install -vv --no-deps --no-build-isolation C:\bazelcrap\%a%\execroot\SnowML\bazel-out\x64_windows-fastbuild\bin\snowflake\ml\snowflake_ml_python-1.0.4-py3-none-any.whl
 
 rem Clean up.
 bazel shutdown
