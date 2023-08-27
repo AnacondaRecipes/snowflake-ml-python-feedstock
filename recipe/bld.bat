7b42bb3304fe7bca7a311cadadb9670c395cf43b
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-08-27 17:46:47 -0500

    add windows support for manual builds, and cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,12 @@
-rem Build the wheel.
-bazel --output_user_root=C:\bazelcrap build --subcommands --verbose_failures --build_python_zip=false --enable_runfiles=true --action_env="USERPROFILE=%USERPROFILE%" --host_action_env="USERPROFILE=%USERPROFILE%" --action_env="CONDA_DLL_SEARCH_MODIFICATION_ENABLE=1" --host_action_env="CONDA_DLL_SEARCH_MODIFICATION_ENABLE=1" //snowflake/ml:wheel
+@echo off
+setlocal EnableDelayedExpansion
 
-rem Install it.
-FOR /F "delims=" %%i IN ('dir C:\bazelcrap /b /ad-h /t:w /od') DO SET a=%%i
-%PYTHON% -m pip install -vv --no-deps --no-build-isolation C:\bazelcrap\%a%\execroot\SnowML\bazel-out\x64_windows-fastbuild\bin\snowflake\ml\snowflake_ml_python-1.0.4-py3-none-any.whl
-
-rem Clean up.
-bazel shutdown
+bazel "--output_user_root=C:\broot" "build" "--repository_cache=" "--nobuild_python_zip" "--enable_runfiles" --action_env="USERPROFILE=%USERPROFILE%" --host_action_env="USERPROFILE=%USERPROFILE%" "//snowflake/ml:wheel"
+SET BAZEL_BIN_PATH=
+FOR /f "delims=" %%a in ('bazel --output_user_root=C:\broot info bazel-bin') DO (SET "BAZEL_BIN_PATH=!BAZEL_BIN_PATH!%%a")
+SET WHEEL_PATH_PATTERN="!BAZEL_BIN_PATH:/=\!\snowflake\ml\*.whl"
+SET WHEEL_PATH=
+FOR /f "delims=" %%a in ('dir /b/s !WHEEL_PATH_PATTERN!') DO (SET "WHEEL_PATH=!WHEEL_PATH!%%a")
+pip "install" "--no-dependencies" "!WHEEL_PATH!"
+bazel "clean" "--expunge"
+bazel "shutdown"
