68880fa3164b792dfd0e0df8df52f7b765cb7e63
Sumit Tiwari <8823156+sumit0190@users.noreply.github.com>
2023-08-27 19:09:56 -0500

    more cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,6 +7,6 @@ FOR /f "delims=" %%a in ('bazel --output_user_root=C:\broot info bazel-bin') DO 
 SET WHEEL_PATH_PATTERN="!BAZEL_BIN_PATH:/=\!\snowflake\ml\*.whl"
 SET WHEEL_PATH=
 FOR /f "delims=" %%a in ('dir /b/s !WHEEL_PATH_PATTERN!') DO (SET "WHEEL_PATH=!WHEEL_PATH!%%a")
-pip "install" "--no-dependencies" "!WHEEL_PATH!"
+pip "install" "--no-dependencies" "--no-build-isolation" "!WHEEL_PATH!"
 bazel "clean" "--expunge"
 bazel "shutdown"
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,3 @@
-# This hack is necessary because bazel produces a Python script with
-# "#! /usr/bin/python3" at the top. On CI systems this is a problem,
-# but only on Linux.
-if [ "$(uname)" == "Linux" ]; then
-    if [ ! -f /usr/bin/python3 ] || [ -L /usr/bin/python3 ]; then
-        rm /usr/bin/python3 || true
-        ln -s ${PYTHON} /usr/bin/python3
-    fi
-fi
-
 # Build the wheel.
 bazel build --subcommands //snowflake/ml:wheel
 
