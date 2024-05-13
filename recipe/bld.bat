2add36d07cc26af47e0295d6da45c8220a0ff397
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-05-13 16:23:49 +0200

    further comments

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,4 +6,8 @@ bazel "--output_user_root=C:\broot" "build" "--repository_cache=" "--nobuild_pyt
 bazel "clean" "--expunge"
 bazel "shutdown"
 
+@REM Some handles are hold by java.exe/micromamba, and prevent conda-build to
+@REM delete/move/rename certain folders and files (e.g. work, and temporary).
+@REM Killing java.exe serves to release such handles and allow conda-build
+@REM to progress with the next builds for different python versions.
 taskkill /IM "java.exe" /F
